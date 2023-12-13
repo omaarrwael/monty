4d7c4566bc746c0e624f1862a46861c1d2de4778
omaarrwael <omar662002@hotmail.com>
2023-12-13 14:41:18 -0800

    monty

---
+++ free.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+/**
+ * free_stack - free
+ * @stack: struct
+ *
+ */
+void free_stack(stack_t *stack)
+{
+stack_t *tmp;
+while (stack)
+{
+tmp = stack;
+stack = stack->next;
+free(tmp);
+}
+}
